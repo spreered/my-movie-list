f099fa37c10bea3fd77bc55a0ca43eec7913ac04
spreered <spreered@gmail.com>
2018-07-30 22:53:46 +0800

    add modal and more btn in index

--- index.js
+++ index.js
@@ -9,7 +9,7 @@
     displayDataList(data)
   }).catch((err) => console.log(err))
 
-  function displayDataList (dataList) {
+  function displayDataList(dataList) {
     const dataPanel = document.getElementById('data-panel')
     let htmlContent = ''
     dataList.forEach(function (item, index) {
@@ -19,6 +19,9 @@
             <img class="card-img-top " src="${POSTER_URL}${item.image}" alt="Card image cap">
             <div class="card-body movie-item-body">
               <h6 class="card-title">${item.title}</h5>
+            </div>
+            <div class="card-footer">
+              <button class="btn btn-primary btn-show-movie" data-toggle="modal" data-target="#show-movie-modal">More</button>
             </div>
           </div>
         </div>
