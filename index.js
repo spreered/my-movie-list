f6d74865dad5d041a8bfa1ee0e1d646ea611202c
spreered <spreered@gmail.com>
2018-07-30 22:49:44 +0800

    movie list card beautify

--- index.js
+++ index.js
@@ -6,6 +6,24 @@
 
   axios.get(INDEX_URL).then((response) => {
     data.push(...response.data.results)
-    console.log(data)
+    displayDataList(data)
   }).catch((err) => console.log(err))
+
+  function displayDataList (dataList) {
+    const dataPanel = document.getElementById('data-panel')
+    let htmlContent = ''
+    dataList.forEach(function (item, index) {
+      htmlContent += `
+        <div class="col-sm-3">
+          <div class="card mb-2">
+            <img class="card-img-top " src="${POSTER_URL}${item.image}" alt="Card image cap">
+            <div class="card-body movie-item-body">
+              <h6 class="card-title">${item.title}</h5>
+            </div>
+          </div>
+        </div>
+      `
+    })
+    dataPanel.innerHTML = htmlContent
+  }
 })()
