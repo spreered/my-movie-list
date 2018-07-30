d2b151fe252af86d2ecbf724884c09f0f11991b7
spreered <spreered@gmail.com>
2018-07-30 22:45:44 +0800

    access movie index api

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 (function () {
-  // write your code here
+  const BASE_URL = 'http://localhost:3000'
+  const INDEX_URL = BASE_URL + '/api/v1/movies/'
+  const POSTER_URL = BASE_URL + '/posters/'
+  const data = []
+
+  axios.get(INDEX_URL).then((response) => {
+    data.push(...response.data.results)
+    console.log(data)
+  }).catch((err) => console.log(err))
 })()
