9f25a32b7754e032ea40a80d7a75cbbda5ca4fc4
spreered <spreered@gmail.com>
2018-07-31 00:22:01 +0800

    add search bar for searching movie title

--- index.js
+++ index.js
@@ -10,6 +10,8 @@
   }).catch((err) => console.log(err))
 
   function displayDataList (dataList) {
+    const searchBtn = document.getElementById('submit-search')
+    const searchInput = document.getElementById('search')
     const dataPanel = document.getElementById('data-panel')
     let htmlContent = ''
     dataList.forEach(function (item, index) {
@@ -34,6 +36,30 @@
       if (event.target.matches('.btn-show-movie')) {
         showMovie(event.target.dataset.id)
       }
+    })
+    searchBtn.addEventListener('click', event => {
+      let resultData = ''
+      event.preventDefault()
+      const regex = RegExp(searchInput.value, 'i')
+
+      dataList.forEach((item, index) => {
+        if (item.title.match(regex)) {
+          resultData += `
+            <div class="col-sm-3">
+              <div class="card mb-2">
+                <img class="card-img-top " src="${POSTER_URL}${item.image}" alt="Card image cap">
+                <div class="card-body movie-item-body">
+                  <h6 class="card-title">${item.title}</h5>
+                </div>
+                <div class="card-footer">
+                  <button class="btn btn-primary btn-show-movie" data-toggle="modal" data-target="#show-movie-modal" data-id="${item.id}">More</button>
+                </div>
+              </div>
+            </div>
+          `
+        }
+      })
+      dataPanel.innerHTML = resultData
     })
 
     function showMovie (movieId) {
