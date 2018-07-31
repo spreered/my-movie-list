fd0cfdc51d8f926a8ba7a119f6afdbc4669811e1
spreered <spreered@gmail.com>
2018-07-31 11:13:12 +0800

    add favorite button on Movie item

--- index.js
+++ index.js
@@ -24,6 +24,7 @@
             </div>
             <div class="card-footer">
               <button class="btn btn-primary btn-show-movie" data-toggle="modal" data-target="#show-movie-modal" data-id="${item.id}">More</button>
+              <button class="btn btn-info btn-add-favorite" data-index="${index}">+</i></button>
             </div>
           </div>
         </div>
@@ -53,6 +54,7 @@
                 </div>
                 <div class="card-footer">
                   <button class="btn btn-primary btn-show-movie" data-toggle="modal" data-target="#show-movie-modal" data-id="${item.id}">More</button>
+                  <button class="btn btn-info btn-add-favorite" data-index="${index}">+</i></button>
                 </div>
               </div>
             </div>
