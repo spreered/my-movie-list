ca80292585e73a6835cf6e89bd3ed8785c62dd4f
spreered <spreered@gmail.com>
2018-07-31 14:04:09 +0800

    add callback function for movie template

--- index.js
+++ index.js
@@ -94,4 +94,21 @@
       localStorage.setItem('favoriteMovie', JSON.stringify(dataStorage))
     }
   }
+
+  function indexItemTemplate (item, index) {
+    return `
+      <div class="col-sm-3">
+        <div class="card mb-2">
+          <img class="card-img-top " src="${POSTER_URL}${item.image}" alt="Card image cap">
+          <div class="card-body movie-item-body">
+            <h6 class="card-title">${item.title}</h5>
+          </div>
+          <div class="card-footer">
+          <button class="btn btn-primary btn-show-movie" data-toggle="modal" data-target="#show-movie-modal" data-id="${item.id}">More</button>
+          <button class="btn btn-info btn-add-favorite" data-index="${index}">+</i></button>
+          </div>
+        </div>
+      </div>
+      `
+  }
 })()
