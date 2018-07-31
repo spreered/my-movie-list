8c0791416bb79fdc235588fc75a62d8e6e6a0c64
spreered <spreered@gmail.com>
2018-07-31 11:39:16 +0800

    init favorite.html and favorite.js

---
+++ favorite.js
@@ -0,0 +1,6 @@
+(function () {
+  const BASE_URL = 'http://localhost:3000'
+  const INDEX_URL = BASE_URL + '/api/v1/movies/'
+  const POSTER_URL = BASE_URL + '/posters/'
+  const data = JSON.parse(localStorage.getItem('favoriteMovie')) || []
+})()
