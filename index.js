3ef30a58756c0c1470355fa78997919984186e0c
spreered <spreered@gmail.com>
2018-08-01 10:47:49 +0800

    add displayDataList function

--- index.js
+++ index.js
@@ -3,9 +3,23 @@
   const INDEX_URL = BASE_URL + '/api/v1/movies/'
   const POSTER_URL = BASE_URL + '/posters/'
   const data = []
+  const dataPanel = document.getElementById('data-panel')
 
   axios.get(INDEX_URL).then((response) => {
     data.push(...response.data.results)
-    console.log(data)
+    displayDataList(data)
   }).catch((err) => console.log(err))
+
+  function displayDataList (dataList) {
+    let htmlContent = ''
+    dataList.forEach(function (item, index) {
+      htmlContent += `
+        <div>
+          <img src="${POSTER_URL}${item.image}">
+          <h6>${item.title}</h6>
+        </div>
+        `
+    })
+    dataPanel.innerHTML = htmlContent
+  }
 })()
