0d28aee5d9ba15049f1bf659db4f7e547c61d287
spreered <spreered@gmail.com>
2018-08-01 13:23:17 +0800

    add displayDataList and showMovie in favorite.js

--- favorite.js
+++ favorite.js
@@ -1,3 +1,53 @@
 (function () {
+  const BASE_URL = 'http://localhost:3000'
+  const INDEX_URL = BASE_URL + '/api/v1/movies/'
+  const POSTER_URL = BASE_URL + '/posters/'
+  const dataPanel = document.getElementById('data-panel')
+  const data = JSON.parse(localStorage.getItem('favoriteMovie')) || []
 
+  displayDataList(data)
+
+  dataPanel.addEventListener('click', (event) => {
+    if (event.target.matches('.btn-show-movie')) {
+      showMovie(event.target.dataset.id)
+    }
+  })
+
+  function displayDataList (dataList) {
+    let htmlContent = ''
+    dataList.forEach(function (item, index) {
+      htmlContent += `
+        <div class="col-sm-3">
+          <div class="card mb-2">
+            <img class="card-img-top " src="${POSTER_URL}${item.image}" alt="Card image cap">
+            <div class="card-body movie-item-body">
+              <h6 class="card-title">${item.title}</h5>
+            </div>
+            <div class="card-footer">
+              <button class="btn btn-primary btn-show-movie" data-toggle="modal" data-target="#show-movie-modal" data-id="${item.id}">More</button>
+              <button class="btn btn-danger btn-remove-favorite" data-id="${item.id}">X</i></button>
+            </div>
+          </div>
+        </div>
+      `
+    })
+    dataPanel.innerHTML = htmlContent
+  }
+
+  function showMovie (movieId) {
+    const modalTitle = document.getElementById('show-movie-title')
+    const modalImage = document.getElementById('show-movie-image')
+    const modalDate = document.getElementById('show-movie-date')
+    const modalDescription = document.getElementById('show-movie-description')
+    const url = INDEX_URL + movieId
+    console.log(url)
+    axios.get(url).then(response => {
+      const data = response.data.results
+      console.log(data)
+      modalTitle.textContent = data.title
+      modalImage.innerHTML = `<img src="${POSTER_URL}${data.image}" class="img-fluid" alt="Responsive image">`
+      modalDate.textContent = `release at : ${data.release_date}`
+      modalDescription.textContent = `${data.description}`
+    })
+  }
 })()
